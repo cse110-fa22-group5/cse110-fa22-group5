653a80467e7b3ed3d45780218e8984a402ef9866
Akanksha Pandey <78574543+akanksha-maker-ucsd@users.noreply.github.com>
2022-11-08 13:42:33 -0800

    added package.json
    helps with jest on GHA

--- jestTesting/sum.test.js
+++ jestTesting/sum.test.js
@@ -3,3 +3,9 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+/*
+sources: 
+https://jestjs.io/docs/getting-started
+https://github.com/marketplace/actions/run-jest
+*/
