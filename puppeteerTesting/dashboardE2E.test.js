f6ece81926be9b926d9c36dfe7f481c5afc5b254
Arlong-Z <ze329366490@gmail.com>
2022-11-28 14:03:21 -0800

    adaption for the changes of main for previous test

--- puppeteerTesting/dashboardE2E.test.js
+++ puppeteerTesting/dashboardE2E.test.js
@@ -71,7 +71,7 @@
   it('Checking to make sure the Note text area has no input initially', async () => {
 
     console.log('Checking to make sure the Note text area has no input initially');
-    const noteTextArea = await page.$eval('#notes-content-input',e => e.value);
+    const noteTextArea = await page.$eval('#edit-content',e => e.value);
     expect(noteTextArea).toBe('');
   }, 2500);
 
@@ -81,11 +81,11 @@
     it('Check to make sure the text area input is changed from empty to 5 lines', async () => {
 
     console.log('Checking to make sure the title input is now "Lecture 1 CSE 110"...');
-    var inputTxt = await page.$('#notes-content-input');
+    var inputTxt = await page.$('#edit-content');
     await inputTxt.click({clickCount: 2});
-    await page.type('#notes-content-input','Lecture 1 CSE 110. ' +
+    await page.type('#edit-content','Lecture 1 CSE 110. ' +
     'Hello this is my first note!');
-    titleText = await page.$eval('#notes-content-input', e => e.value);
+    titleText = await page.$eval('#edit-content', e => e.value);
     expect(titleText).toBe('Lecture 1 CSE 110. Hello this is my first note!');
 
   }, 100000);
@@ -95,10 +95,12 @@
   it('Check to make sure "Save" button should redirect to the note view editor mode window', async () => {
     
     console.log('Checking "Save" button...');
-    const newButton = await page.$('button');
+    const newButton = await page.$('#save-button');
     await newButton.click();
     await page.waitForNavigation(); 
-    expect(page.url()).toBe('https://cse110-fa22-group5.github.io/cse110-fa22-group5/source/notes.html?id=1$preview=true');
+    expect(page.url()).toBe('https://cse110-fa22-group5.github.io/cse110-fa22-group5/source/notes.html?id=1');
+    const titleEditable = await page.$eval('#title-input',e => e.disabled);
+    expect(titleEditable).toBe(true);
 
   }, 2500);
 
@@ -108,20 +110,28 @@
     it('Check to make sure after clicking Save the window is in view editor mode and user cannot edit note page', async () => {
 
     console.log('Checking title input uneditable...');
-    const titleText = await page.$eval('#notes-title',e => e.innerHTML);
+    const titleText = await page.$eval('#title-input',e => e.value);
     console.log(titleText);
+    const titleEditable = await page.$eval('#title-input',e => e.disabled);
     expect(titleText).toBe('Lecture 1 CSE 110');
+    expect(titleEditable).toBe(true);
+    var editTxtOff = await page.$eval('#edit-content', e => e.hidden);
+    var viewTxtOff = await page.$eval('#view-content', e => e.hidden);
+    expect(editTxtOff).toBe(true);
+    expect(viewTxtOff).toBe(false);
     
   }, 2500);
 
 /**
  * Check to make sure the element note-content-input attribute 'disabled' exists to indicate no editing note body
  */
-  it('Check to make sure the note content body "disabled" attribute is on', async () => {
+  it('Checking that editcontext is hidden and viewcontext is not', async () => {
 
-  console.log('Checking that note content input disabled attribute is on...');
-  const notesDisabled = await page.$eval('#notes-content-input',e => e.getAttribute('disabled'));
-  expect(notesDisabled).toBe('disabled');
+  console.log('Checking that editcontext is hidden and viewcontext is not...');
+  var editTxtOff = await page.$eval('#edit-content', e => e.hidden);
+  var viewTxtOff = await page.$eval('#view-content', e => e.hidden);
+  expect(editTxtOff).toBe(true);
+  expect(viewTxtOff).toBe(false);
   
 }, 2500);  
 
@@ -131,7 +141,7 @@
    it('Check to make sure the "Edit button" is on the page on view mode ', async () => {
 
     console.log('Making sure the "Edit button" is on the page on view mode ...');
-    const editButton = await page.$('button');
+    const editButton = await page.$('#change-view-button');
     const editB = await editButton.getProperty('innerText');
     const editBVal = await editB.jsonValue();
     expect(editBVal).toBe('Edit');
@@ -144,12 +154,11 @@
     it('Check to make sure when you click the "Edit" button, title is now editable', async () => {
 
     console.log('Check to make sure title is now editable ...');
-    const editButton = await page.$('button');
+    const editButton = await page.$('#change-view-button');
     await editButton.click();
-    await page.waitForNavigation();
-    const titleText = await page.$eval('#notes-title',e => e.innerText);
-    console.log(titleText);
-    expect(titleText).toBe('Title: ');
+    const titleOff = await page.$eval('#title-input',e => e.disabled);
+    console.log(titleOff);
+    expect(titleOff).toBe(false);
     
     
   }, 2500);  
@@ -172,25 +181,29 @@
 */
     it('Check to make sure note body is changed', async () => {
 
-    var inputTxt = await page.$('#notes-content-input');
+    var inputTxt = await page.$('#edit-content');
     await inputTxt.click({clickCount: 1});
-    await page.type('#notes-content-input',' Adding discussion text');
-    var titleText = await page.$eval('#notes-content-input', e => e.value);
+    await page.type('#edit-content',' Adding discussion text');
+    var titleText = await page.$eval('#edit-content', e => e.value);
     expect(titleText).toBe('Lecture 1 CSE 110. Hello this is my first note! Adding discussion text');
     
   }, 10000);  
 
   
 /**
-* Clicking "Save note" to go back to view mode, check preview in the url is false
+* Clicking "Save note" to go back to view mode
 */
-it('Clicking "Save note" to go back to view mode, check preview in the url is false', async () => {
+it('Clicking "Save note" to go back to view mode', async () => {
 
-console.log('Checking window url from "Save" button...');
-const newButton = await page.$('button');
+console.log('Checking mode from "Save" button...');
+const newButton = await page.$('#save-button');
 await newButton.click();
-await page.waitForNavigation(); 
-expect(page.url()).toBe('https://cse110-fa22-group5.github.io/cse110-fa22-group5/source/notes.html?id=1$preview=true');
+let editTxt = await page.$('#edit-content');
+let viewTxt = await page.$('#view-content');
+var editTxtOff = await page.$eval('#edit-content', e => e.hidden);
+var viewTxtOff = await page.$eval('#view-content', e => e.hidden);
+expect(editTxtOff).toBe(true);
+expect(viewTxtOff).toBe(false);
 
 }, 10000); 
 
@@ -219,26 +232,7 @@ it('Click "Back" button to be redirected to the note Dashboard url', async () =>
     expect(numNotes).toBe(1);
 
   }, 2500);
-
-   /**
-   *  Check that our local indexedDB database is populated with the current note
-   * 
   
-    it('Check that our local indexedDB database is populated with the current note', async () => {
-
-      console.log('Checking local storage (indexedDB)...');
-
-      const localStorage = await page.evaluate( async () => {
-        const db = indexedDB.open('NotesDB',1);
-        db.onsuccess = function (e) {
-          console.log(db.result);
-        }
-      });
-    
-      console.log(localStorage);
-      expect(localStorage).toEqual([]);
-    }, 2500);
-*/
     /**
      *  Check if delete button work
      */
